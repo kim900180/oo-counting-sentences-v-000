4417a71ba7374c48eb3e5406e35733164d40895e
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 19:46:15 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,14 +19,8 @@ class String
     i = 0
     counter = 0
     # binding.pry
-    while i < count.length
-      if count[i] != ""
-        counter += 1
-        i += 1
-      elsif count[i] == ""
-        counter = counter
-        i += 1
-      end
+    count.each do |item|
+      counter += 1 unless item == ""
     end
     counter
   end
