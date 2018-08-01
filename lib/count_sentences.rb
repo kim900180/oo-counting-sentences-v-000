68ab95f5f8b68217116c99e881b8116adce2303e
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 19:38:08 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.split(/[!,.,?]/)
+    i = 0
+    counter = 0
+    binding.pry
+    while i < count.length
+      if count[i] != ""
+        counter += 1
+        i += 1
+      else
+        counter = counter
+        i += 1
+      end
+    end
+    counter
   end
 end
